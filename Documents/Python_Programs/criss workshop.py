c03ea82e6065eefad60179b200089597dae42a60
Abhimanyu Bhowmik <abhimanyu.bhowmik1@gmail.com>
2024-01-16 08:56:41 -0800

    Revert "commit success"
    This reverts commit a7f8e45cb488ab6ba974d1afefcaf4b21806c761.

--- Documents/Python_Programs/criss workshop.py
+++
@@ -1,8 +0,0 @@
-import socket
-serverIP = '172.17.29.11'
-serverPORT = 6000
-
-serveraddress = (serverIP, serverPORT)
-bufferSize = 1024
-
-UDPClientSocket = socket.socket(family.socket.AF_INET, tyoe = socket)
